476d21358147d4b59261f6198077076b279683cb
Felix ITs 01 <felixits01@felixs-macbook-pro-2.local>
2017-04-08 11:49:40 +0530

    Initial Commit

---
+++ sdCore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sdCore
+//
+//  Created by Felix ITs 01 on 08/04/17.
+//  Copyright © 2017 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sdCore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sdCore
+//
+//  Created by Felix ITs 01 on 08/04/17.
+//  Copyright © 2017 Felix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
